c15f0d2e5ccdc5c7706b8e95febf0fe0c99f639b
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2024-01-23 05:00:41 +0600

    house-hunter-sever-side setup completed

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT | 3004
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = process.env.DB_USER;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello House Hunters!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
